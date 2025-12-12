4ff2c35af566e908ada820e69bdf8ed5aab70d25
Ashlee <ashlee.tolentino8@gmail.com>
2025-12-12 01:44:42 -0500

    updated my campuses.js file

--- src/store/reducers/campuses.js
+++ src/store/reducers/campuses.js
@@ -1,19 +1,29 @@
 /*==================================================
 /src/store/reducers/campuses.js
 
-This is a Reducer function that accepts 2 parameters: the previous state object (aka current state) and an action object. 
-Depending on the Action object, the Reducer updates the State and return the new State object.
-It also defines the State and its default initial value.
+Reducer for the list of all campuses.
 ================================================== */
-import * as at from "../actions/actionTypes";  // Import Action Types ("at" keyword for Action Type)
+import * as at from "../actions/actionTypes";
 
-// REDUCER:
-const allCampuses = (state = [], action) => {  // Empty array as default Initial State
+const allCampuses = (state = [], action) => {
   switch (action.type) {
     case at.FETCH_ALL_CAMPUSES:
       return action.payload;
+
+    case at.ADD_CAMPUS:
+      return [...state, action.payload];
+
+    case at.DELETE_CAMPUS:
+      // payload is campusId
+      return state.filter((campus) => campus.id !== action.payload);
+
+    case at.EDIT_CAMPUS:
+      // payload is updated campus object
+      return state.map((campus) =>
+        campus.id === action.payload.id ? action.payload : campus
+      );
+
     default:
-      // If the Reducer doesn't recognize the Action Type, returns the previous (current) State unchanged.
       return state;
   }
 };
